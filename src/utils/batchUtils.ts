82628168ffa64281495761b30a3e21fb9c96c1c9
dexter826 <2001222641@hufi.edu.vn>
2026-02-03 19:23:57 +0700

    feat: remove unused utility functions from batchUtils, imageUtils, and uploadUtils

--- src/utils/batchUtils.ts
+++ src/utils/batchUtils.ts
@@ -44,29 +44,3 @@ export const batchGetUsers = async (userIds: string[]): Promise<Record<string, U
   }
 };
 
-export const debounce = <T extends (...args: any[]) => any>(
-  func: T,
-  wait: number
-): ((...args: Parameters<T>) => void) => {
-  let timeout: NodeJS.Timeout | null = null;
-
-  return (...args: Parameters<T>) => {
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), wait);
-  };
-};
-
-export const throttle = <T extends (...args: any[]) => any>(
-  func: T,
-  limit: number
-): ((...args: Parameters<T>) => void) => {
-  let inThrottle: boolean;
-  
-  return (...args: Parameters<T>) => {
-    if (!inThrottle) {
-      func(...args);
-      inThrottle = true;
-      setTimeout(() => (inThrottle = false), limit);
-    }
-  };
-};
